d3b88be30c2beea6d16848e95a622cb9909cf220
Pavel Shvets <pavel.v.shvets@gmail.com>
2026-03-10 18:08:35 -0600

    changed Class names and imports according to the changes in the names of the files, added movement logic to SpaceFlight class

--- flight_among_asteroids_arcade_game.py
+++ flight_among_asteroids_arcade_game.py
@@ -3,13 +3,13 @@
 # Imports
 import arcade
 import random
-from carRide import CarRide
+from spaceFlight import SpaceFlight
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_TITLE, SCALING
 
 
 if __name__ == "__main__":
-    # Create a new Car Ride window
-    car_ride_game = CarRide(
+    # Create a new Space Flight window
+    car_ride_game = SpaceFlight(
         int(SCREEN_WIDTH * SCALING), int(SCREEN_HEIGHT * SCALING), SCREEN_TITLE
     )
     # Setu to play
--- spaceFlight.py
+++ spaceFlight.py
@@ -6,7 +6,7 @@ from constants import SCALING
 
 
 
-class CarRide(arcade.Window):
+class SpaceFlight(arcade.Window):
     """Space Shooter side scroller game
     Player starts on the left, enemies appear on the right
     Player can move anywhere, but not off screen
@@ -14,7 +14,7 @@ class CarRide(arcade.Window):
     Collisions end the game
     """
 
-    """Car Ride game
+    """Space Flight Among Asteroids game
     Player starts on the center and moves along a road
     Player can move within the road, but not off road
     Obstacles appear on the road at variable speed
@@ -51,6 +51,7 @@ class CarRide(arcade.Window):
         self.planet = arcade.Sprite("images/planet03.png", SCALING)
         self.planet.center_x = self.width * 1.8
         self.planet.bottom = -500
+        self.planet_rotation_speed = 0.2 # degrees per frame
         self.all_sprites.append(self.planet)
 
         # Set up the player
@@ -102,10 +103,66 @@ class CarRide(arcade.Window):
         if self.player.left < 0:
             self.player.left = 0
 
+        # Rotating the planet
+        self.planet.angle -= self.planet_rotation_speed
+
     def on_draw(self):
         """
             Draw all game objects
         """
         self.clear()
         self.all_sprites.draw()
+
+    def on_key_press(self, symbol, modifiers):
+        """
+            Handle user keyboard input 
+            Q: Quit the game
+            P: Pause/Unpause the game
+            I/J/K/L: Move Up, Left, Down, Right
+            Arrows: Move Up, Left, Down, Right
+        """
+        if symbol == arcade.key.Q:
+            # Quit immediately
+            arcade.close_window()
+
+        if symbol == arcade.key.P:
+            # Quit immediately
+            self.paused = not self.paused
+
+        if symbol == arcade.key.P:
+            # Quit immediately
+            self.paused = not self.paused
+
+        if symbol == arcade.key.I or symbol == arcade.key.UP:
+            self.player.change_y = 5  
+
+        if symbol == arcade.key.K or symbol == arcade.key.DOWN:
+            self.player.change_y = -5  
+
+        if symbol == arcade.key.J or symbol == arcade.key.LEFT:
+            self.player.change_x = -5  
+
+        if symbol == arcade.key.L or symbol == arcade.key.RIGHT:
+            self.player.change_x = 5 
+    
+    def on_key_release(self, symbol, modifiers):
+        """
+            Stop movement when movement keys are realeased
+        """
+
+        if(
+            symbol == arcade.key.I
+            or symbol == arcade.key.K
+            or symbol == arcade.key.UP
+            or symbol == arcade.key.DOWN
+        ):
+            self.player.change_y = 0
+
+        if(
+            symbol == arcade.key.J
+            or symbol == arcade.key.L
+            or symbol == arcade.key.LEFT
+            or symbol == arcade.key.RIGHT
+        ):
+            self.player.change_x = 0
         
